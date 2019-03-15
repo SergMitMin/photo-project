7c3a851e7d467d3287589fb17600e392dacd8d2b
SergMitMin <35815763+SergMitMin@users.noreply.github.com>
2019-03-15 18:03:29 +0300

    docs: "add project main files"

---
+++ js/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	$('.photo-wrapper').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			// titleSrc: function(item) {
+			// 	return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			// }
+    },
+    zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+	$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+		disableOn: 300,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+});
