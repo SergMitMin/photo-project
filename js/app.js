cc219eca21c7ce36c3035645ecd30358796740ae
Siarhei Mitasau <darkenRahl1893@gmail.com>
2019-03-17 23:44:14 +0300

    feat: add mobile menu

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	$('.photo-wrapper').magnificPopup({
 		delegate: 'a',
 		type: 'image',
 		tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+		mainClass: 'mfp-with-zoom', // this class is for CSS animation below
 		gallery: {
 			enabled: true,
 			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+			preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
 		},
 		image: {
 			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
 			// titleSrc: function(item) {
 			// 	return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
 			// }
-    },
-    zoom: {
+		},
+		zoom: {
 			enabled: true,
 			duration: 300 // don't foget to change the duration also in CSS
 		}
@@ -28,5 +28,10 @@ $(document).ready(function() {
 		preloader: false,
 
 		fixedContentPos: false
+	});
+	$('.open-popup-link').magnificPopup({
+		type: 'inline',
+		midClick: true,
+		mainClass: 'my-mfp-zoom-in'
 	});
 });
